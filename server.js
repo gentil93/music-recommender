d348b31eea6d36111f9043673045730c471d5260
Gentil <matheusg93@gmail.com>
2018-05-07 19:12:33 -0300

    heroku

--- server.js
+++ server.js
@@ -3,13 +3,14 @@ var path = require('path');
 var express = require('express');
 
 var app = express();
+var port = process.env.PORT || 5000
 
 const DIST_DIR = path.join(__dirname, './dist'),
       HTML_FILE = path.join(DIST_DIR, "../public/index.html");
 app.use(express.static(DIST_DIR));
-app.set('port', process.env.PORT || 8080);
+app.set('port', port);
 app.get("/", (req, res) => res.sendFile(HTML_FILE));
 
-var server = app.listen(app.get('port'), function() {
+var server = app.listen(port,"0.0.0.0", function() {
   console.log('listening on port ', server.address().port);
 });

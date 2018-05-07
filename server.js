44c2cf031ec3d2dc13e3e095975505253fbcac9c
Gentil <matheusg93@gmail.com>
2018-05-07 18:46:32 -0300

    zannox

--- server.js
+++ server.js
@@ -4,8 +4,11 @@ var express = require('express');
 
 var app = express();
 
-app.use(express.static(path.join(__dirname, 'dist')));
+const DIST_DIR = path.join(__dirname, './dist'),
+      HTML_FILE = path.join(DIST_DIR, "../public/index.html");
+app.use(express.static(DIST_DIR));
 app.set('port', process.env.PORT || 8080);
+app.get("/", (req, res) => res.sendFile(HTML_FILE));
 
 var server = app.listen(app.get('port'), function() {
   console.log('listening on port ', server.address().port);
